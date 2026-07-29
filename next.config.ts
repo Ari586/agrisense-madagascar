8ada1c71de484f218858843c0f1b6ec46424c349
Ari586 <la7ariel@gmail.com>
2026-07-29 23:42:06 +0200

    fix: remove deprecated eslint config

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: false,
   devIndicators: false,
   async rewrites() {
